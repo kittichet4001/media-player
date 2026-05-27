cc61940882c1362854f8255c690770e739bb242f
kittichet4001 <omexo4001@gmail.com>
2026-05-27 17:58:34 +0700

    edit command youtube music search engine

--- search/music.bat
+++ search/music.bat
@@ -1,1 +1,4 @@
-mpv "ytdl://https://music.youtube.com/search?q=BTS"
+set /p "song_name= Song or Artist to you want listen: "
+
+if "%song_name%"=="" goto search_loop
+mpv "ytdl://https://music.youtube.com/search?q=%song_name%"
